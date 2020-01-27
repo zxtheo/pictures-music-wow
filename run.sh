bfa4bbd5844b59a4802f5f38409baa646e732531
zxtheo <1709869@brunel.ac.uk>
2020-01-27 11:25:37 +0000

    moved images into images folder

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
-python main.py doge.jpg b g 180 &
-python main.py doge.jpg g b 180
-
-
+python main.py doge.jpg b g 200 &
+python main.py doge.jpg g r 200
--- service/image_service.py
+++ service/image_service.py
@@ -4,6 +4,6 @@ from PIL import Image
 from data.image_data import ImageData
 
 def get_image_data(image):
-    im = Image.open("data/{}".format(image), 'r')
+    im = Image.open("data/images/{}".format(image), 'r')
     pix_val = list(im.getdata())
     return ImageData(pix_val)
