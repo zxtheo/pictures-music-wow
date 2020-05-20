3fd360bf1a4229cdae9bb4b86fae80087f1042b4
Andy Juniper <andy_juniper@mcafee.com>
2020-05-20 20:38:21 +0100

    fix it

--- run.sh
+++ run.sh
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-# python main.py doge.jpg r b 300 &
-python main.py pepe.jpg b g 180
-=======
 #!/bin/bash
 python3 main.py ${1:-doge.jpg} g b 120 ${2:--}
->>>>>>> 46dfc3c6c0369459791b5ab89479f62ecb4ddcae
